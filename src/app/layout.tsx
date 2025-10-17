c95a217c68a9b10e56cb0d8e09553da84e5e2fe9
lmJesterAndDev <subotinmaksim7@gmail.com>
2025-10-17 19:49:02 +0300

    layount2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "PrismArc",
   description: "Made with Love",
+  icons: {
+    icon: "/prismarc-logo.png",
+  },
 };
 
 export default function RootLayout({
