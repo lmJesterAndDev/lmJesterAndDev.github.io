33e46cc006b8972fc1a48048cd78f1820df7932f
lmJesterAndDev <subotinmaksim7@gmail.com>
2025-10-17 18:31:25 +0300

    Updates

---
+++ _next/static/chunks/22f670df3a404313.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/12cded59bcc9a7c2.js",
+  "static/chunks/e82dcd802f0798ff.js",
+  "static/chunks/turbopack-3538df54842601f3.js"
+])
---
+++ _next/static/chunks/92b2ea521943242a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/12cbe93021cdb5f5.js",
+  "static/chunks/e82dcd802f0798ff.js",
+  "static/chunks/turbopack-9f7530428ff7dfeb.js"
+])
